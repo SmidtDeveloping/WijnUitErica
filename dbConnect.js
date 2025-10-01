359942261f10034ec7c5973936a9011d8db6b73d
SmidtDeveloping <101642623+SmidtDeveloping@users.noreply.github.com>
2025-10-01 09:37:09 +0200

    DB Connect Error Oplossing #2

--- dbConnect.js
+++ dbConnect.js
@@ -2,10 +2,9 @@ const mongoose = require("mongoose");
 
 async function connect(mongoURL) {
 
-    await mongoose.connect(mongoURL, {
+    mongoose.connect(mongoURL, {
         serverSelectionTimeoutMS: 5000,
         socketTimeoutMS: 45000, 
-        bufferCommands: false, 
         autoIndex: false, 
 
     }).then(
